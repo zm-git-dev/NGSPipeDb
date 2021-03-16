9b97d9bf2e1eb4db4eded068640eaa0d7fecc7e9
xuanblo <296373256@qq.com>
2021-03-16 19:19:17 +0800

    delete script in wooey [skip ci]

--- ngspipe/rules/8.db_generate_addscript.Snakefile.py
+++ ngspipe/rules/8.db_generate_addscript.Snakefile.py
@@ -14,9 +14,12 @@ rule add_script2wooey:
     benchmark:
         join(addscript_outdir, "benchmark.txt")
     params:
-        dna2rna_path = join('ngsdb', 'wooey', 'wooey_scripts', 'dna2rna.py'),
+        dna2rna_path = join('ngsdb', 'wooey', 'wooey_scripts'),
         dna2rna_name = 'dna2rna_convert',
     shell:
         '''
-        python ngsdb/manage.py addscript {params.dna2rna_path} --name {params.dna2rna_name} 1>{log} 2>&1;
+        # remove scripts in wooey database
+        python ngspipe/scripts/remove_script_in_wooey_db.py 1>{log} 2>&1
+        # add scripts in current machine
+        python ngsdb/manage.py addscript {params.dna2rna_path} --name {params.dna2rna_name} 1>>{log} 2>&1;
         '''
